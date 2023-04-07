465e266c7afd77a3ea6ed1ce7dd7303241ab1eaf
Mirko Robbi <mirko.robbi@gmail.com>
2023-04-07 16:02:07 +0200

    ADD security

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,3 +9,13 @@ INSERT INTO offers (offer_start_date, offer_end_date, offer_name, pizza_id) VALU
 INSERT INTO ingredients (name) VALUES('pomodorino');
 INSERT INTO ingredients (name) VALUES('mozzarella');
 INSERT INTO ingredients (name) VALUES('farina');
+
+
+INSERT INTO users (email, first_name, last_name, password) VALUES('mirko@email.it', 'Mirko', 'Robbi', '{noop}mirko');
+INSERT INTO users (email, first_name, last_name, password) VALUES('benny@email.it', 'Benedetta', 'Scattolin','{noop}benny');
+
+INSERT INTO roles (id, name) VALUES(1, 'ADMIN');
+INSERT INTO roles (id, name) VALUES(2, 'USER');
+
+INSERT into users_roles(user_id, roles_id) VALUES(1, 1);
+INSERT into users_roles(user_id, roles_id) VALUES(2, 2);
